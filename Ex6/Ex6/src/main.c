952332434f4b3692e37de54abb87a2be525a1504
Anders Vedel Eriksen <s194017@student.dtu.dk>
2020-06-10 11:42:53 +0200

    Ex6 Update Main

--- Ex6/Ex6/src/main.c
+++ Ex6/Ex6/src/main.c
@@ -25,12 +25,14 @@ static uint8_t hundreths = 0;
 static uint8_t seconds = 0;
 static uint8_t minutes = 0;
 static uint8_t hours = 0;
+static uint8_t printSec = 0;
 
 void TIM2_IRQHandler(void) {
     hundreths++;
     if (hundreths >= 100) {
         hundreths = 0;
         seconds++;
+        printSec = 1;
     }
 
     if (seconds >= 60) {
@@ -50,6 +52,9 @@ void TIM2_IRQHandler(void) {
 int main(void) {
     uart_init(9600);
 
+    printf("\n\n"); //space i terminalen
+    printf("%d.%d.%d.%d\n", hours, minutes, seconds, hundreths);//0.0.0.0
+
     RCC->APB1ENR |= RCC_APB1Periph_TIM2; //Enable Clockline to timer 2.
     TIM2->CR1 = 0x01; //Timer enabled, all other bits disabled
     TIM2->ARR = 0xFF; //Reload value sat til 256-1.
@@ -62,21 +67,12 @@ int main(void) {
     /* Udregninger:
     Periodetid = (1+ARR)*(1+PSC) / ClockFreq = 10ms, 1/100sekund
     */
-    int16_t sec = 0;
-    uint8_t lastSec = 0;
 
-    printf("%d.%d.%d.%d\n", hours, minutes, seconds, hundreths);//0.0.0.0
   while(1)
   {
-    //sec = second(TIM2, sec);
-    if(seconds > lastSec){
-        lastSec = seconds;
+    if(printSec) {
+        printSec = 0;
         printf("%d.%d.%d.%d\n", hours, minutes, seconds, hundreths);
     }
-
-    if(lastSec >= 59){
-        lastSec = 0;
-    }
-
   }
 }
