ca8551a3d3c123fe4626b07ffbb0f2dbe25599ea
xzores <jakobfu@hotmail.com>
2020-06-20 12:24:56 +0200

    mappping movement

--- Common/Utility.h
+++ Common/Utility.h
@@ -21,6 +21,9 @@
 #define GREEN_LED GPIOC, 7
 #define RED_LED GPIOB, 4
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
 //self explanatory
 inline int32_t power(int32_t a, int32_t exp) {
 // calculates a^exp
@@ -35,6 +38,6 @@ void setPortPuPd(GPIO_TypeDef * GPIO, char pin, uint8_t pupd);
 uint8_t readPortPin(GPIO_TypeDef * GPIO, char pin);
 void setPin(GPIO_TypeDef * GPIO, char pin, uint8_t val);
 void setLED(uint8_t color);
-uint8_t readJoystick(); 
+uint8_t readJoystick();
 void setupTimer(TIM_TypeDef* TIM, uint32_t RCC_APBPeriph, uint16_t reloadValue, uint16_t prescale);
 void setupTimerInterupts(TIM_TypeDef* TIM, uint8_t TIM_IRQn,  uint8_t priority);
--- Mapping/Mapping/src/main.c
+++ Mapping/Mapping/src/main.c
@@ -15,13 +15,25 @@
 #include <stdlib.h>
 #include "30010_io.h"
 #include "Utility.h"
+#include <string.h>
 
 struct Map{
 
     uint8_t* buffer;
+    char* renderBuffer;
     uint16_t mySize;
     char style[4][3];
     uint8_t colors[4];
+};
+
+
+struct Player{
+
+    int16_t x;
+    int16_t y;
+    char look;
+    uint8_t viewSize;
+
 };
 
 
@@ -107,62 +119,73 @@ uint8_t tileScheme(char* toPrint, uint8_t t, uint8_t style) {
     }
 
 
-void printSubMap(struct Map* myMap, uint8_t x, uint8_t y, uint8_t sizeX, uint8_t sizeY) {
+void printSubMap(struct Map* myMap, struct Player* player, uint8_t x, uint8_t y, uint8_t sizeX, uint8_t sizeY) {
 
     uint8_t i, j;
+    uint32_t m = 0;
+    uint8_t lastTile = -1; //ye, this is legal
 
-
-    char toPrint[sizeX * 10 + 2]; // *10 ??
-
-
+    char toPrint[sizeX * 30 + 2]; // *10 ??
 
 
     for (j = y; j < y + sizeY; j++) {
-        uint8_t t = 0;
+        uint16_t t = 0;
+        uint8_t xin = x;
 
-        for (i = x / 4; i < (x + sizeX) / 4 + 1; i++){
+        for (i = x ; i < x + sizeX; i++){
 
-            uint8_t data = myMap->buffer[j * myMap->mySize + i];
+            uint8_t data = myMap->buffer[j * myMap->mySize + i / 4];
 
-            uint8_t d1, d2, d3, d4;
-            d1 = (data & (3 << 0)) >> 0;
-            d2 = (data & (3 << 2)) >> 2;
-            d3 = (data & (3 << 4)) >> 4;
-            d4 = (data & (3 << 6)) >> 6;
+            uint8_t s = ((i % 3) * 2);
+            uint8_t d = (data & (3 << s)) >> s;
+
+            srand(i + j);
 
             uint8_t r = rand() % 3;
 
-            t = tileScheme(&toPrint, t, d1); //ESC-codes tilføjes til toPrint, t opdateres
-            toPrint[t++] = myMap->style[d1][r];
-            if (d2 != d1)
-            t = tileScheme(&toPrint, t, d2);
-            toPrint[t++] = myMap->style[d2][r];
-            if (d3 != d2)
-            t = tileScheme(&toPrint, t, d3);
-            toPrint[t++] = myMap->style[d3][r];
-            if (d4 != d3)
-            t = tileScheme(&toPrint, t, d4);
-            toPrint[t++] = myMap->style[d4][r];
+            //print the tile, optimazation, dont print new escape char if they are allready there
+            if (d != lastTile){
+                t = tileScheme(toPrint, t, d);
+                lastTile = d;
+            }
+
+            //print tile or player
+            if(j == player->y && xin == player->x){
+                //print the player
+                toPrint[t++] = player->look;
+            }
+            else{
+                toPrint[t++] = myMap->style[d][r];
+            }
+
+            xin++;
+
         }
 
         toPrint[t++] = '\n';
-        toPrint[t++] = '\0';
 
-        printf(toPrint);
+        memcpy(&(myMap->renderBuffer[m]), toPrint, t);
+        m += t;
     };
+
+    myMap->renderBuffer[m + 1] = '\0';
 }
 
+volatile int a = 0;
 
+static uint8_t mem[100*100];
 
 int main(void)
 {
-    srand(10321);
 
     uart_init(115200);
     homeCurser(); //Sæt curser til 0,0
     clearTermninal(); // Ryd terminal
+
+    struct Player player = {10, 10, 'P', 10};
+
     //struct Map myMap = {malloc(50 * 50 * sizeof(uint8_t)), 50, {{0xB0,0xB0,0xB0},  {0xDB,0xDB,0xDB}, {0xB3,0xDD,0xEF}, {0xF4, 0xF4, 0xF4}}, {1,2,3,4}};
-    struct Map myMap = {malloc(50 * 50 * sizeof(uint8_t)), 50, {{0xDB,0xDB,0xDB},  {0xB2,0xB2,0xB2}, {0xF7,0xF7,0xF7}, {0xF4, 0xF4, 0xF4}}, {1,2,3,4}}; //R, W, G, L
+    struct Map myMap = {mem, calloc(30 * player.viewSize * player.viewSize * sizeof(char), sizeof(uint8_t)), 50, {{0xB0,0xB0,0xB0},  {0xDB,0xDB,0xDB}, {0xB3,0xDD,0xEF}, {0xF4, 0xF4, 0xF4}}, {1,2,3,4}}; //R, W, G, L
     memset(myMap.buffer, 0x00, 50 * 50);
     buildMap(&myMap, 0, 0, 5, 10, 'G');
     buildMap(&myMap, 5, 0, 4, 10, 'R');
@@ -170,9 +193,71 @@ int main(void)
     buildMap(&myMap, 10, 1, 8, 8, 'W');
 
 
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOA; //Enabling clock for IO Port A
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOD;
+
+    setPortMode(RIGHT_JOY_STICK, IN_MODE); //Porten RJS er forbundet til sættes til input-mode.
+    setPortPuPd(RIGHT_JOY_STICK, NO_PULL); //Porten er forbundet til 1/0 i hardware, det er ikke nødvendigt med pull ?? Anders
+
+    setPortMode(UP_JOY_STICK, IN_MODE);
+    setPortPuPd(UP_JOY_STICK, NO_PULL);
+
+    setPortMode(CENTER_JOY_STICK, IN_MODE);
+    setPortPuPd(CENTER_JOY_STICK, NO_PULL);
+
+    setPortMode(LEFT_JOY_STICK, IN_MODE);
+    setPortPuPd(LEFT_JOY_STICK, NO_PULL);
+
+    setPortMode(DOWN_JOY_STICK, IN_MODE);
+    setPortPuPd(DOWN_JOY_STICK, NO_PULL);
+
     while(1)
     {
-        printSubMap(&myMap, 0,0, 20,10);
+
+        uint8_t joy = readJoystick();
+
+        if(joy & 0x1){
+            //up
+            player.y--;
+        }
+        if(joy & 0x2){
+            //up
+            player.y++;
+        }
+        if(joy & 0x4){
+            //up
+            player.x--;
+        }
+        if(joy & 0x8){
+            //up
+            player.x++;
+        }
+
+        player.x = MAX(player.x,  0);
+        player.y = MAX(player.y,  0);
+        player.x = MIN(player.x,  myMap.mySize - 1);
+        player.y = MIN(player.y,  myMap.mySize - 1);
+
+        //the position used as the players position (we max out at the bounderies of the map)
+        uint16_t showX = MIN(myMap.mySize - 1 - player.viewSize, MAX(0, player.x - player.viewSize));
+        uint16_t showY = MIN(myMap.mySize - 1 - player.viewSize, MAX(0, player.y - player.viewSize));
+
+        //render position, where should we render the player
+        uint16_t rPosX = MIN(myMap.mySize - 1, MAX(0, player.x - player.viewSize));
+        uint16_t rPosY = MIN(myMap.mySize - 1, MAX(0, player.y - player.viewSize));
+
+        printSubMap(&myMap, &player, showX, showY, player.viewSize * 2, player.viewSize * 2);
         gotoxy(0,0);
+        printf(myMap.renderBuffer);
+
+        int i = 0;
+        for(i = 0; i < 10000; i++){
+            a = i + i;
+        }
+
+
+
     };
 }
